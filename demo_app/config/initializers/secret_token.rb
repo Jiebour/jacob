30832d8b44fea833859ebaf1375efbe646a1a3ba
macworld <macworld@macworld-virtual-machine.(none)>
2013-01-19 07:01:17 +0800

    inital commit

---
+++ demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'bf83f260bc1245dee5a00ea46f2cde02c8c582557ed054d8732b33659030943f81312e734e0b96bc76f5bed3b2d083439121d1303c905b3282de48cb6f15be79'
